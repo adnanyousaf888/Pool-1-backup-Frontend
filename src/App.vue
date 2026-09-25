db67b7159ac4186976865493a0ef7be178458615
Adnan Yousaf <adnanyousaf1357@gmail.com>
2026-09-25 15:40:49 +0200

    added code

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,8 @@
 import { ref } from 'vue'
 import User from './components/User.vue'
 import ClockManager from './components/ClockManager.vue'
-import WorkingTimes from './components/WorkingTimes.vue'
-import WorkingTime from './components/WorkingTime.vue'
-import ChartManager from './components/ChartManager.vue'
 
 const currentUser = ref(null)
-const workingTimesRef = ref(null)
-const chartManagerRef = ref(null)
-
-function refreshAll() {
-  workingTimesRef.value?.getWorkingTimes()
-  chartManagerRef.value?.loadData()
-}
-
-async function handleDelete(id) {
-  await fetch(`http://localhost:4000/api/workingtime/${id}`, { method: 'DELETE' })
-  refreshAll()
-}
 </script>
 
 <template>
@@ -29,9 +14,6 @@ async function handleDelete(id) {
 
     <div v-if="currentUser">
       <ClockManager :userId="currentUser.id" />
-      <WorkingTime :userId="currentUser.id" @saved="refreshAll" />
-      <WorkingTimes ref="workingTimesRef" :userId="currentUser.id" @delete-clicked="handleDelete" />
-      <ChartManager ref="chartManagerRef" :userId="currentUser.id" />
     </div>
   </div>
 </template>
