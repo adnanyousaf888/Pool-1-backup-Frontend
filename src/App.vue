1897bbad1f1e1106585245dfa872c39e112034dc
Adnan Yousaf <adnanyousaf1357@gmail.com>
2026-09-25 15:24:07 +0200

    Initial project

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from 'vue'
+
+const currentUser = ref(null)
+</script>
+
+<template>
+  <div id="app">
+    <h1>Time Manager</h1>
+  </div>
+</template>
+
+<style scoped>
+</style>
